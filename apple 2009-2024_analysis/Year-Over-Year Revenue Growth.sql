6ae2cd2894e1b61ee32e33ee278271ebdaf9fc64
SaulRollins97 <saulrollins22@gmail.com>
2025-05-20 16:18:56 -0400

    Update Year-Over-Year Revenue Growth.sql

--- apple 2009-2024_analysis/Year-Over-Year Revenue Growth.sql
+++ apple 2009-2024_analysis/Year-Over-Year Revenue Growth.sql
@@ -4,4 +4,4 @@ SELECT
     ROUND(Revenue - LAG(Revenue) OVER (ORDER BY Year), 2) AS YoY_Revenue_Change,
     ROUND((Revenue - LAG(Revenue) OVER (ORDER BY Year)) / LAG(Revenue) OVER (ORDER BY Year) * 100, 2) AS YoY_Growth_Percent
 FROM 
-    apple_2009-2024;
+    Apple_20092024;
